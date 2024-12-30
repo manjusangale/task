086c3091566f73605a4adf9f7f994571d3cafa7f
manjusangale <manjusangale13@gmail.com>
2024-12-30 13:48:30 +0000

    new

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "ap-south-1"
-  access_key = "AKIAS252VZJO2ZO7CWUF"
-  secret_key = "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"
+  access_key = "AKIAS252VZJO5ZFBP5KE"
+  secret_key = "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"
 }
 
 
